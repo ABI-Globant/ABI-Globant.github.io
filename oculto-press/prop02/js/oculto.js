cea4ddfb4e83209c7f626a53d0a1e73b9431e52b
Fernando Moreno <fernando.moreno@globant.com>
2015-02-04 11:24:02 -0300

    #oculto - fix styles prop02

--- oculto-press/prop02/js/oculto.js
+++ oculto-press/prop02/js/oculto.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
   var maskSite = $('.mask-site'),
-    imgBg = $('.img-bg');  
+    imgBg = $('.img-bg'),  
+    imgSmoke = $('.img-smoke');
   document.onmousemove = function(e) {
     var x = -(e.clientX/10);
     var y = -(e.clientY/10);
     maskSite.animate({left: x, top: y},{duration: 50, queue: false, easing: 'linear'});
-    imgBg.animate({'background-position-x': x/3, 'background-position-y': y/3},{duration: 50, queue: false, easing: 'linear'});
+    imgBg.animate({'background-position-x': x/5, 'background-position-y': y/5},{duration: 50, queue: false, easing: 'linear'});
+    imgSmoke.animate({left: x/2, bottom: y/2-50},{duration: 50, queue: false, easing: 'linear'});
   }; 
 });
 
