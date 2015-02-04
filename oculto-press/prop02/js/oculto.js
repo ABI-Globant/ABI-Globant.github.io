33d145f0ebde7b5610646e9fc94f47a693a4e75d
Fernando Moreno <fernando.moreno@globant.com>
2015-02-04 09:47:08 -0300

    #oculto - fix prop02

--- oculto-press/prop02/js/oculto.js
+++ oculto-press/prop02/js/oculto.js
@@ -1,46 +1,13 @@
-var intCurrentContainer = 1;
-var intMaxContainerCount = 3;
-
-function startVideoSwaps() {
-
-	// Every so often fade to next video
-	setInterval (function() {
-
-		// Determine next container to display
-		var intNextContainer = intCurrentContainer + 1;
-		if( intNextContainer > intMaxContainerCount ){
-			intNextContainer = 1
-		}
-
-		// Swap containers
-		swapContainers( intCurrentContainer, intNextContainer );
-
-		// Update new current container
-		intCurrentContainer = intNextContainer;
-
-	}, 3000 );
-
-}
-
-function swapContainers( intContainerOut, intContainerIn ) {
-
-	console.log( "Out:" + intContainerOut + " In:" + intContainerIn );
-
-	$( "#divVideoContainer" + intContainerOut ).fadeOut( 300 );
-	$( "#divVideoContainer" + intContainerIn ).fadeIn( 300 );
-
-
-}
-
 $(document).ready(function () {
-	startVideoSwaps();
+  var maskSite = $('.mask-site'),
+    imgBg = $('.img-bg');  
   document.onmousemove = function(e) {
     var x = -(e.clientX/10);
     var y = -(e.clientY/10);
-
-    this.body.style.backgroundPosition = x + 'px ' + y + 'px';
-    $('.mask-site').css('left', x + 'px ')
-  };
+    maskSite.animate({left: x, top: y},{duration: 50, queue: false, easing: 'linear'});
+    imgBg.animate({'background-position-x': x/3, 'background-position-y': y/3},{duration: 50, queue: false, easing: 'linear'});
+  }; 
 });
+
 
 
